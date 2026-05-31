c424ae921561a7d681d305de841b394d9394a5a4
pranotimohite <pranoti.mohite@gmail.com>
2026-05-31 13:40:05 +0200

    Springboot application main class files.

---
+++ auth-service/src/main/java/com/carbontrack/auth_service/AuthServiceApplication.java
@@ -0,0 +1,14 @@
+package com.carbontrack.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
